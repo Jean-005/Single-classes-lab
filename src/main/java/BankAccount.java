cc156c8235a49747323a0bd29b4e99feba0ebc5e
Jean <jean-ata@hotmail.co.uk>
2024-02-08 00:11:43 +0000

    added getters and setters

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -8,6 +8,45 @@ public class BankAccount {
     String accountNumber;
     int balance;
 
-//    
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+    
 
 }
