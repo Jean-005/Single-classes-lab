6987b9c6c39e1879a941d0271ec8e19e3b23bc1c
Jean <jean-ata@hotmail.co.uk>
2024-02-08 01:29:22 +0000

    initial testing for methods

--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -4,11 +4,33 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class BankAccountTest {
 
+    private BankAccount bankAccount;
+
     @Test
     public void testGettersAndSetters() {
-        BankAccount account = new BankAccount();
 
-        account.setFirstName("Jean");
-        assertThat().isEqualTo("Jean");
+
+        BankAccount account = new BankAccount("Tim", "Smith" 2005-10-10, "15387654", 0);
+
+//        Tests
+        
+        assertThat(bankAccount.getFirstName()).isEqualTo("Tim");
+
+
+
+        assertThat(bankAccount.getLastName()).isEqualTo("Smith");
+
+
+
+        assertThat(bankAccount.getDateOfBirth()).isEqualTo(2005-10-10);
+
+
+
+        assertThat(bankAccount.getAccountNumber()).isEqualTo("15387654");
+
+
+
+        assertThat(bankAccount.getBalance()).isEqualTo(0);
+
     }
 }
