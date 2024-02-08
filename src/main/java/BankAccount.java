24f26c162b4195e52f0157aba6da62e4e906ca31
Jean <jean-ata@hotmail.co.uk>
2024-02-08 00:05:25 +0000

    first commit

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,2 +1,13 @@
+import java.time.LocalDate;
+
 public class BankAccount {
+
+    String firstName;
+    String lastName;
+    LocalDate dateOfBirth;
+    String accountNumber;
+    int balance;
+
+//    
+
 }
